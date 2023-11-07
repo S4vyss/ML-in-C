19e5610793427b3661dc92ce240bd3418e57c3f3
S4vyss <mlodywojownik345@gmail.com>
2023-11-07 20:46:15 +0100

    Fixing bug

--- NeuronKacpra.h
+++ NeuronKacpra.h
@@ -15,30 +15,57 @@
 #define NN_ASSERT assert
 #endif // NN_ASSERT
 
-#define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
+#define ARRAY_LEN(xs) sizeof((xs)) / sizeof((xs)[0])
+
+float rand_float(void);
+float sigmoidf(float x);
 
 typedef struct {
   size_t rows;
   size_t cols;
-  float *es;
   size_t stride;
+  float *es;
 } Mat;
 
 #define MAT_AT(m, i, j) (m).es[(i) * (m).stride + (j)]
+
+Mat mat_alloc(size_t rows, size_t cols);
+void mat_fill(Mat m, float x);
+void mat_rand(Mat m, float low, float high);
+Mat mat_row(Mat m, size_t row);
+void mat_copy(Mat dst, Mat src);
+void mat_dot(Mat dst, Mat a, Mat b);
+void mat_sum(Mat dst, Mat a);
+void mat_sig(Mat m);
+void mat_print(Mat m, const char *name, size_t padding);
 #define MAT_PRINT(m) mat_print(m, #m, 0)
-#define NN_PRINT(m) nn_print(m, #m)
-#define NN_INPUT(nn) (nn).as[0]
-#define NN_OUTPUT(nn) (nn).as[(nn).count]
 
 typedef struct {
   size_t count;
   Mat *ws;
   Mat *bs;
-  Mat *as;
+  Mat *as; // The amount of activations is count+1
 } NN;
 
-// size_t arch[] = {2, 2, 1};
-// NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+#define NN_INPUT(nn) (nn).as[0]
+#define NN_OUTPUT(nn) (nn).as[(nn).count]
+
+NN nn_alloc(size_t *arch, size_t arch_count);
+void nn_print(NN nn, const char *name);
+#define NN_PRINT(nn) nn_print(nn, #nn);
+void nn_rand(NN nn, float low, float high);
+void nn_forward(NN nn);
+float nn_cost(NN nn, Mat ti, Mat to);
+void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to);
+void nn_learn(NN nn, NN g, float rate);
+
+#endif // NN_H_
+
+#ifdef NN_IMPLEMENTATION
+
+float sigmoidf(float x) { return 1.f / (1.f + expf(-x)); }
+
+float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
 
 Mat mat_alloc(size_t rows, size_t cols) {
   Mat m;
@@ -48,14 +75,6 @@ Mat mat_alloc(size_t rows, size_t cols) {
   m.es = NN_MALLOC(sizeof(*m.es) * rows * cols);
   NN_ASSERT(m.es != NULL);
   return m;
-}
-
-void mat_fill(Mat m, float x) {
-  for (size_t i = 0; i < m.rows; ++i) {
-    for (size_t j = 0; j < m.cols; ++j) {
-      MAT_AT(m, i, j) = x;
-    }
-  }
 }
 
 void mat_dot(Mat dst, Mat a, Mat b) {
@@ -68,50 +87,8 @@ void mat_dot(Mat dst, Mat a, Mat b) {
     for (size_t j = 0; j < dst.cols; ++j) {
       MAT_AT(dst, i, j) = 0;
       for (size_t k = 0; k < n; ++k) {
-        MAT_AT(dst, i, j) = MAT_AT(a, i, k) * MAT_AT(b, k, j);
+        MAT_AT(dst, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);
       }
-    }
-  }
-}
-
-void mat_sum(Mat dst, Mat a) {
-  NN_ASSERT(dst.rows == a.rows);
-  NN_ASSERT(dst.cols == a.cols);
-  for (size_t i = 0; i < dst.rows; ++i) {
-    for (size_t j = 0; j < dst.cols; ++j) {
-      MAT_AT(dst, i, j) += MAT_AT(a, i, j);
-    }
-  }
-}
-
-void mat_print(Mat m, const char *name, size_t padding) {
-  printf("%*s%s = [\n", (int)padding, "", name);
-  for (size_t i = 0; i < m.rows; ++i) {
-    printf("%*s  ", (int)padding, "");
-    for (size_t j = 0; j < m.cols; ++j) {
-      printf("%f ", MAT_AT(m, i, j));
-    }
-    printf("\n");
-  }
-  printf("%*s]\n", (int)padding, "");
-}
-
-float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
-
-void mat_rand(Mat m, float low, float high) {
-  for (size_t i = 0; i < m.rows; ++i) {
-    for (size_t j = 0; j < m.cols; ++j) {
-      MAT_AT(m, i, j) = rand_float() * (high - low) + low;
-    }
-  }
-}
-
-float sigmoidf(float x) { return 1.f / (1.f + expf(-x)); }
-
-void mat_sig(Mat m) {
-  for (size_t i = 0; i < m.rows; ++i) {
-    for (size_t j = 0; j < m.cols; ++j) {
-      MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
     }
   }
 }
@@ -128,7 +105,6 @@ Mat mat_row(Mat m, size_t row) {
 void mat_copy(Mat dst, Mat src) {
   NN_ASSERT(dst.rows == src.rows);
   NN_ASSERT(dst.cols == src.cols);
-
   for (size_t i = 0; i < dst.rows; ++i) {
     for (size_t j = 0; j < dst.cols; ++j) {
       MAT_AT(dst, i, j) = MAT_AT(src, i, j);
@@ -136,8 +112,56 @@ void mat_copy(Mat dst, Mat src) {
   }
 }
 
-NN nn_alloc(size_t *arch, size_t arch_count) {
+void mat_sum(Mat dst, Mat a) {
+  NN_ASSERT(dst.rows == a.rows);
+  NN_ASSERT(dst.cols == a.cols);
+  for (size_t i = 0; i < dst.rows; ++i) {
+    for (size_t j = 0; j < dst.cols; ++j) {
+      MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+    }
+  }
+}
 
+void mat_sig(Mat m) {
+  for (size_t i = 0; i < m.rows; ++i) {
+    for (size_t j = 0; j < m.cols; ++j) {
+      MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
+    }
+  }
+}
+
+void mat_print(Mat m, const char *name, size_t padding) {
+  printf("%*s%s = [\n", (int)padding, "", name);
+  for (size_t i = 0; i < m.rows; ++i) {
+    printf("%*s    ", (int)padding, "");
+    for (size_t j = 0; j < m.cols; ++j) {
+      printf("%f ", MAT_AT(m, i, j));
+    }
+    printf("\n");
+  }
+  printf("%*s]\n", (int)padding, "");
+}
+
+void mat_fill(Mat m, float x) {
+  for (size_t i = 0; i < m.rows; ++i) {
+    for (size_t j = 0; j < m.cols; ++j) {
+      MAT_AT(m, i, j) = x;
+    }
+  }
+}
+
+void mat_rand(Mat m, float low, float high) {
+  for (size_t i = 0; i < m.rows; ++i) {
+    for (size_t j = 0; j < m.cols; ++j) {
+      MAT_AT(m, i, j) = rand_float() * (high - low) + low;
+    }
+  }
+}
+
+// size_t arch[] = {2, 2, 1};
+// NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+
+NN nn_alloc(size_t *arch, size_t arch_count) {
   NN_ASSERT(arch_count > 0);
 
   NN nn;
@@ -162,15 +186,12 @@ NN nn_alloc(size_t *arch, size_t arch_count) {
 
 void nn_print(NN nn, const char *name) {
   char buf[256];
-
   printf("%s = [\n", name);
-  Mat *ws = nn.ws;
-  Mat *bs = nn.bs;
   for (size_t i = 0; i < nn.count; ++i) {
-    snprintf(buf, sizeof(buf), "ws%zu", i + 1);
-    mat_print(ws[i], buf, 4);
-    snprintf(buf, sizeof(buf), "bs%zu", i + 1);
-    mat_print(bs[i], buf, 4);
+    snprintf(buf, sizeof(buf), "ws%zu", i);
+    mat_print(nn.ws[i], buf, 4);
+    snprintf(buf, sizeof(buf), "bs%zu", i);
+    mat_print(nn.bs[i], buf, 4);
   }
   printf("]\n");
 }
@@ -183,7 +204,7 @@ void nn_rand(NN nn, float low, float high) {
 }
 
 void nn_forward(NN nn) {
-  for (size_t i = 1; i < nn.count; ++i) {
+  for (size_t i = 0; i < nn.count; ++i) {
     mat_dot(nn.as[i + 1], nn.as[i], nn.ws[i]);
     mat_sum(nn.as[i + 1], nn.bs[i]);
     mat_sig(nn.as[i + 1]);
@@ -208,12 +229,14 @@ float nn_cost(NN nn, Mat ti, Mat to) {
       c += d * d;
     }
   }
+
   return c / n;
 }
 
 void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to) {
   float saved;
   float c = nn_cost(nn, ti, to);
+
   for (size_t i = 0; i < nn.count; ++i) {
     for (size_t j = 0; j < nn.ws[i].rows; ++j) {
       for (size_t k = 0; k < nn.ws[i].cols; ++k) {
@@ -223,6 +246,7 @@ void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to) {
         MAT_AT(nn.ws[i], j, k) = saved;
       }
     }
+
     for (size_t j = 0; j < nn.bs[i].rows; ++j) {
       for (size_t k = 0; k < nn.bs[i].cols; ++k) {
         saved = MAT_AT(nn.bs[i], j, k);
@@ -241,6 +265,7 @@ void nn_learn(NN nn, NN g, float rate) {
         MAT_AT(nn.ws[i], j, k) -= rate * MAT_AT(g.ws[i], j, k);
       }
     }
+
     for (size_t j = 0; j < nn.bs[i].rows; ++j) {
       for (size_t k = 0; k < nn.bs[i].cols; ++k) {
         MAT_AT(nn.bs[i], j, k) -= rate * MAT_AT(g.bs[i], j, k);
@@ -249,8 +274,4 @@ void nn_learn(NN nn, NN g, float rate) {
   }
 }
 
-#endif // !NN_H_
-
-#ifdef NN_IMPLEMENTATION
-
-#endif // NN_IMPLEMENTATIONI
+#endif // NN_IMPLEMENTATION
