30fde4091befdfa4ad197332e56bb920fd9ae3b8
S4vyss <mlodywojownik345@gmail.com>
2023-11-15 21:00:01 +0100

    Linear Regression Done with sgd

--- my_implementation/GoingDeep.c
+++ my_implementation/GoingDeep.c
@@ -48,73 +48,65 @@ float *ln_gradient_descent(int rows, int n, float X[rows][n], float *y,
 
   int cols = n + 1;
   float X_b[rows][cols];
+  float eta = 0.1;
 
   for (size_t i = 0; i < rows; ++i) {
     for (size_t j = 0; j < cols; ++j) {
       X_b[i][j] = X[i][j];
-      X_b[i][cols - 1] = 1.0f;
     }
+    X_b[i][cols - 1] = 1.0f;
   }
-
-  ln_print(cols, X_b, rows, cols);
 
   float theta[2] = {rand_float(), rand_float()};
 
   assert((sizeof(theta) / sizeof(theta[0])) == cols);
 
-  float X_b_dot_theta[rows];
+  for (size_t i = 0; i < 1000; ++i) {
 
-  for (size_t i = 0; i < rows; ++i) {
-    X_b_dot_theta[i] = 0.0f;
-  }
+    float X_b_dot_theta[rows];
+    float X_b_T[cols][rows];
 
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      X_b_dot_theta[i] += X_b[i][j] * theta[j];
+    for (size_t i = 0; i < rows; ++i) {
+      X_b_dot_theta[i] = 0.0f;
+      for (size_t j = 0; j < cols; ++j) {
+        X_b_dot_theta[i] += X_b[i][j] * theta[j];
+      }
+    }
+
+    for (size_t i = 0; i < cols; ++i) {
+      for (size_t j = 0; j < rows; ++j) {
+        X_b_T[i][j] = 0.0f;
+        X_b_T[i][j] = X_b[j][i];
+      }
+    }
+
+    int T_rows = sizeof(X_b_T) / sizeof(X_b_T[0]);
+    int T_cols = sizeof(X_b_T[0]) / sizeof(X_b_T[0][0]);
+    int X_b_dot_rows = sizeof(X_b_dot_theta) / sizeof(X_b_dot_theta[0]);
+
+    assert((sizeof(X_b_dot_theta) / sizeof(X_b_dot_theta[0])) == T_cols);
+
+    // (T_rows x T_cols) * (X_b_dot_rows x X_b_dot_cols) = ()
+    float T_dot_product[T_rows];
+
+    for (size_t i = 0; i < T_rows; ++i) {
+      T_dot_product[i] = 0.0f;
+      for (size_t j = 0; j < T_cols; ++j) {
+        T_dot_product[i] += X_b_T[i][j] * (X_b_dot_theta[j] - y[j]);
+      }
+    }
+
+    float gradients[T_rows];
+
+    for (size_t i = 0; i < T_rows; ++i) {
+      gradients[i] = (2 / (float)train_count) * T_dot_product[i];
+      theta[i] = theta[i] - eta * gradients[i];
     }
   }
+  float temp = theta[0];
+  theta[0] = theta[1];
+  theta[1] = temp;
 
-  float X_b_T[cols][rows];
-
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      X_b_T[j][i] = 0.0f;
-    }
-  }
-
-  for (size_t i = 0; i < cols; ++i) {
-    for (size_t j = 0; j < rows; ++j) {
-      X_b_T[i][j] = X_b[j][i];
-    }
-  }
-
-  int T_rows = sizeof(X_b_T) / sizeof(X_b_T[0]);
-  int T_cols = sizeof(X_b_T[0]) / sizeof(X_b_T[0][0]);
-  int X_b_dot_rows = sizeof(X_b_dot_theta) / sizeof(X_b_dot_theta[0]);
-
-  ln_print(T_cols, X_b_T, T_rows, T_cols);
-
-  assert((sizeof(X_b_dot_theta) / sizeof(X_b_dot_theta[0])) == T_cols);
-
-  // (T_rows x T_cols) * (X_b_dot_rows x X_b_dot_cols) = ()
-  float T_dot_product[T_rows];
-
-  for (size_t i = 0; i < T_rows; ++i) {
-    T_dot_product[i] = 0.0f;
-  }
-
-  for (size_t i = 0; i < T_rows; ++i) {
-    for (size_t j = 0; j < T_cols; ++j) {
-      T_dot_product[i] += X_b_T[i][j] * X_b_dot_theta[j];
-    }
-  }
-
-  for (size_t i = 0; i < 2; ++i) {
-    theta[i] = (2 / 10) * T_dot_product[i];
-  }
-
-  for (size_t i = 0; i < T_rows; ++i) {
-    printf("%f\n", T_dot_product[i]);
-  }
+  return *theta;
 }
 float rand_float() { return (float)rand() / (float)RAND_MAX; }
--- my_implementation/playground.c
+++ my_implementation/playground.c
@@ -5,7 +5,7 @@
 int main() {
 
   srand(time(0));
-  size_t N = 10;
+  size_t N = 100;
 
   float X[N][1];
   float y[N];
@@ -18,7 +18,9 @@ int main() {
   int rows = sizeof(X) / sizeof(X[0]);
   int cols = sizeof(X[0]) / sizeof(X[0][0]);
 
-  ln_gradient_descent(rows, cols, X, y, N);
+  float result[2] = ln_gradient_descent(rows, cols, X, y, N);
+
+  printf("%f, %f\n", result[0], result[1]);
 
   // float *result =
   //     ln_gradient_descent(X, y, rows, cols, train_count, 100 * 1000);
